24bc50484651dd9630a792080c0f381a9c256087
LucasAMBR <lucas.silvaambrosio_9119@outlook.com>
2025-06-16 21:02:50 -0300

    tela de task semi finalizada

--- src/pages/team_details/Team.jsx
+++ src/pages/team_details/Team.jsx
@@ -3,50 +3,122 @@ import style from "./Team.module.css";
 import { useNavigate, useParams } from "react-router-dom";
 import api from "../../api/Api";
 import AddBoxIcon from '@mui/icons-material/AddBox';
+import { useAuth } from "../../contexts/AuthContext";
+import { InsertManuallyModal } from "./components/InsertManuallyModal";
 
 export const Team = () => {
 
     const { teamId } = useParams();
 
+    const { userId, userRole } = useAuth();
+
     const [loading, setLoading] = useState(false);
 
-    const [ projectList, setProjectList ] = useState([]);
+    const [ taskList, setTaskList ] = useState([]);
+    const [ teamData, setTeamData ] = useState({});
+    const [ membersData, setMembersData ] = useState([]);
 
-    const [ createProjectModalOpen, setCreateProjectModalOpen ] = useState(false);
+    const [ addManuallyModalIsOpen, setAddManuallyModalIsOpen ] = useState(false);
 
     const navigate = useNavigate();
 
-    const fetchProjectList = async() => {
-        try{
-            const response = await api.get(`/project/all-my-projects`);
+    const formatDate = (date) => {
+        const data = new Date(date);
 
-            setProjectList(response.data);
+        const dia = String(data.getDate()).padStart(2, '0');
+        const mes = String(data.getMonth() + 1).padStart(2, '0');
+        const ano = data.getFullYear();
+        const hora = String(data.getHours()).padStart(2, '0');
+        const minutos = String(data.getMinutes()).padStart(2, '0');
+
+        return `${dia}/${mes}/${ano} ${hora}:${minutos}`;
+    }
+
+    const fetchTaskListAndTeamData = async() => {
+        try{
+            const response = await api.get(`/task/equip/${teamId}`);
+            const teamData = await api.get(`/equip/${teamId}`);
+            const membersData = await api.get(`/dev/equip/${teamId}/all`);
+
+            setTaskList(response.data);
+            setTeamData(teamData.data);
+            setMembersData(membersData.data)
         }catch(error){
             console.error(error);
         }
     }
 
     useEffect(() => {
-        
+        fetchTaskListAndTeamData()
     }, [])
 
     return(
         <div className={style.page_content}>
             <h2>Departamento da equipe</h2>
             <p className={style.project_desc}>Detalhes do projeto</p>
-            <div className={style.button_wrapper}>
-                <button>Create Task</button>
-                <button>Generate Invite code</button>
-            </div>
+            {userId == teamData.leaderId && 
+                <div>
+                    <button className={style.button_task}>Create Task</button>
+                    <button className={style.button_task} onClick={() => setAddManuallyModalIsOpen(true)}>Insert member manually</button>
+                    <button className={style.button_task}>Generate Invite code</button>
+                </div>
+            }
+
+            {userRole == "MNG" &&
+                <div>
+                    <button className={style.button_task}>Create Task</button>
+                    <button className={style.button_task} onClick={() => setAddManuallyModalIsOpen(true)}>Insert member manually</button>
+                    <button className={style.button_task}>Generate Invite code</button>
+                </div>
+            }
             <h2>Tarefas</h2>
-            <div className={style.project_list} style={projectList.length == 0 ? {display: "flex", justifyContent: "center", alignItems: "center"} : {}}>
+            <div className={style.project_list}>
                 <div className={style.projects}>
-                    <div className={style.task_table_header}>...</div>
+                    <div className={style.task_table_header} onClick={() => console.log(teamData)}>Tasks</div>
+                    {taskList.map((item, index) => (
+                        <div className={style.task_table_item}>
+                            <p>{item.description}</p>
+                            <p>{formatDate(item.initialDate)}</p>
+                            <p>{formatDate(item.finalDate)}</p>
+                            <p>{item.assigneeId == null ? "Free" : item.assigneeName}</p>
+                            {userRole == "MNG" && 
+                                <>
+                                    <button className={style.assign_button}>Atribuir</button>
+                                </>
+                            }
+                            {userId == teamData.leaderId && userRole == "DEV" && 
+                                <div>
+                                    <button className={style.assign_button}>Pegar tarefa</button>
+                                    <button className={style.assign_button}>Atribuir</button>
+                                </div>
+                            }
+                            {item.assigneeId == null &&
+                                <>
+                                    {userId != teamData.leaderId && userRole != "MNG" &&
+                                        <button className={style.assign_button}>Pegar tarefa</button>
+                                    }
+                                </>
+                            }
+                        </div>
+                    ))}
                 </div>
                 <div className={style.devs}>
-                    {teamId}
+                    <div className={style.devs_table_header}>
+                        Members
+                    </div>
+                    {membersData.map((item,index)=> (
+                        <div className={style.dev_table_item}>
+                            <p className={style.devId}>{item.id}</p>
+                            <p>{item.name}</p>
+                        </div>
+                    ))
+                        
+                    }
                 </div>
         </div>
+        {addManuallyModalIsOpen &&
+            <InsertManuallyModal fetchTasks={fetchTaskListAndTeamData} teamId={teamId} setModal={setAddManuallyModalIsOpen}/>
+        }
         </div>
     )
 }
---
+++ src/pages/team_details/components/InsertManuallyModal.jsx
@@ -0,0 +1,47 @@
+import style from "./InsertManuallyModal.module.css"
+import ComputerIcon from '@mui/icons-material/Computer';
+
+import { useState } from "react";
+import api from "../../../api/Api";
+
+export const InsertManuallyModal = ({ fetchTasks, teamId, setModal }) => {
+
+    const [devIdInput, setDevIdInput ] = useState("");
+
+    const handleDevIdInput = (e) => {
+        setDevIdInput(e.target.value);
+    }
+
+    const handleSuccess = () => {
+        fetchTasks();
+        setModal(false);
+    }
+
+    const handleAddAction = async() => {
+        try{
+            const response = await api.post(`/equip/${teamId}/add/${devIdInput}`);
+
+            handleSuccess()
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    return(
+        <div className={style.modal_area}>
+            <div className={style.modal_content}>
+                <h2>Update team data</h2>
+                <form>
+                    <div className={style.input_wrapper}>
+                        <div className={style.input_icon}> <ComputerIcon sx={{fill: "#ffffff", width: "20px"}} /> </div>
+                        <input type="text" placeholder="Member Id" value={devIdInput} onChange={handleDevIdInput}/>
+                    </div>
+                    <div className={style.button_wrapper}>
+                        <button className={style.login_submit} onClick={handleAddAction}>Add</button>
+                        <button className={style.alt_button} onClick={()=> setModal(false)}>Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
